7cec5ec75d81393221cafcc94e4e23a9c1b4b6b3
3imed-jaberi <imed_jebari@hotmail.fr>
2020-01-28 00:27:07 +0100

    cleanup 🎯 ..

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 
-module.exports = isJSON
-
 /**
  * Check if `body` should be interpreted as json.
+ *
  */
 
-function isJSON (body) {
-  if (!body) return false
-  if (typeof body === 'string') return false
-  if (typeof body.pipe === 'function') return false
-  if (Buffer.isBuffer(body)) return false
-  return true
+module.exports = function (body) {
+  return !(
+    !body ||
+    typeof body === 'string' ||
+    typeof body.pipe === 'function'
+  )
 }
