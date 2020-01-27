265561b0202cfcc603c755a47bede61839c7c22c
3imed-jaberi <imed_jebari@hotmail.fr>
2020-01-28 00:24:00 +0100

    run lint --eslint 💅🏻 ..

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 
-module.exports = isJSON;
+module.exports = isJSON
 
 /**
  * Check if `body` should be interpreted as json.
  */
 
-function isJSON(body) {
-  if (!body) return false;
-  if ('string' == typeof body) return false;
-  if ('function' == typeof body.pipe) return false;
-  if (Buffer.isBuffer(body)) return false;
-  return true;
+function isJSON (body) {
+  if (!body) return false
+  if (typeof body === 'string') return false
+  if (typeof body.pipe === 'function') return false
+  if (Buffer.isBuffer(body)) return false
+  return true
 }
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,37 +1,35 @@
 
-const assert = require('assert');
-const isJSON = require('..');
-
+const assert = require('assert')
+const isJSON = require('..')
 
 describe('koa-is-json test', () => {
   let body
 
   it('Condition 1: !body', () => {
-    assert.ok(!isJSON());
-  });
+    assert.ok(!isJSON())
+  })
 
   it('Condition 2: \'string\' == typeof body', () => {
     body = 'hello world !'
-    assert.ok(!isJSON(body));
-  });
+    assert.ok(!isJSON(body))
+  })
 
   it('Condition 3: \'function\' == typeof body.pipe', () => {
     body = {
       pipe: () => {
-        return 'hello world !';
+        return 'hello world !'
       }
-    };
+    }
 
-    assert.ok(!isJSON(body));
-  });
+    assert.ok(!isJSON(body))
+  })
 
   it('Condition 4: check null', () => {
-    assert.ok(!isJSON(null));
-  });
+    assert.ok(!isJSON(null))
+  })
 
   it('Condition 5: check correct body', () => {
-    body = JSON.parse(JSON.stringify({ msg: 'hello world !' }));
-    assert.ok(isJSON(body));
-  });
-
-});
+    body = JSON.parse(JSON.stringify({ msg: 'hello world !' }))
+    assert.ok(isJSON(body))
+  })
+})
