effc6ed5da2808862b7bdb44e3898a3a7b23f904
imed jaberi <imed_jebari@hotmail.fr>
2020-02-05 05:39:50 +0100

    Fix and Update 🚀 ! (#4)
    *  update .gitignore file 🐞 ..
    
    *  disable package-lock.json 🚫 ..
    
    *  add test cases ☢️ ..
    
    *  add eslint to pkg.json --lint 💄 ..
    
    *  run lint --eslint 💅🏻 ..
    
    *  cleanup 🎯 ..
    
    *  add koa contributors to license 🌍 ..
    
    *  add test coverage 📦 ..
    
    *  update README.md 📋 ..
    
    *  add CI pipeline 📦 ..
    
    *  better stuctrue 🎗 ..
    
    * add method name --'isJSON' 🚀 ..
    
    *  add missing condition 🚀 ..
    
    *  add test case ☢️ ..

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
-# Koa Is JSON
+# [**koa-is-json**](https://github.com/koajs/is-json)
 
 [![Greenkeeper badge](https://badges.greenkeeper.io/koajs/is-json.svg)](https://greenkeeper.io/)
 
-Check if a body is JSON
+> Check if a body is JSON.
+
+
+## License
+
+[MIT](/LICENSE)
--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 
-module.exports = isJSON;
-
 /**
  * Check if `body` should be interpreted as json.
+ *
  */
 
-function isJSON(body) {
-  if (!body) return false;
-  if ('string' == typeof body) return false;
-  if ('function' == typeof body.pipe) return false;
-  if (Buffer.isBuffer(body)) return false;
-  return true;
+module.exports = function isJSON (body) {
+  return !(
+    !body ||
+    typeof body === 'string' ||
+    typeof body.pipe === 'function' ||
+    Buffer.isBuffer(body)
+  )
 }
